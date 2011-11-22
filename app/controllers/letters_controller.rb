2b63524a2b4ac51e866f13625eb2eda8b56533c8
Edward Castano <ed.castano@gmail.com>
2011-11-21 19:25:08 -0800

    Moved logic from views to controller

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -25,23 +25,49 @@ class LettersController < ApplicationController
 
   @emphasis = params[:emphasis]
   if @emphasis != nil
-    @emphasis = params[:emphasis]
+      @emphasis = params[:emphasis]
 	  @political_ideology = params[:ideology]
 	  @religion = params[:religion]
 	  @ideologyKey = @political_ideology + @religion
 	  @geography = params[:geography]
-    @recipient = params[:recipient]
+      @recipient = params[:recipient]
   else
-    @recipient = " Representative"
+    @recipient_lastname = " Representative"
   end
+ 
+	@congresspeople = Sunlight::Legislator.all_for(:address => @address)
+	@senior_senator = @congresspeople[:senior_senator]
+	@junior_senator = @congresspeople[:junior_senator]
+	@representative = @congresspeople[:representative]
 
-  @congresspeople = Sunlight::Legislator.all_for(:address => @address)
-  @senior_senator = @congresspeople[:senior_senator]
-  @junior_senator = @congresspeople[:junior_senator]
-  @representative = @congresspeople[:representative]
+    @sen_senator_title = 'Senator '+@senior_senator.firstname + " " + @senior_senator.lastname 
+	@jun_senator_title = 'Senator '+@junior_senator.firstname + " " + @junior_senator.lastname
+	@rep_title = 'Representative '+@representative.firstname + " " + @representative.lastname 
 
+	@sen_senator_address = @senior_senator.congress_office 
+	@jun_senator_address = @junior_senator.congress_office
+	@rep_address = @representative.congress_office 
+
+	@sen_senator_title_lastname = 'Senator' + ' ' + @senior_senator.lastname 
+	@jun_senator_title_lastname = 'Senator' + ' ' + @junior_senator.lastname 
+	@rep_title_lastname = 'Representative' + ' ' + @representative.lastname 
+
+	@sen_senator_name = @senior_senator.firstname + " " + @senior_senator.lastname 
+	@jun_senator_name = @junior_senator.firstname + " " + @junior_senator.lastname 
+	@rep_name = @representative.firstname + " " + @representative.lastname 
+
+	@rep_address = Hash[[[@sen_senator_title, @sen_senator_address], [@jun_senator_title, @jun_senator_address], [@rep_title, @rep_address]]]  
+	@rep_lastname = Hash[[[@sen_senator_title, @sen_senator_title_lastname], [@jun_senator_title, @jun_senator_title_lastname], [@rep_title, @rep_title_lastname]]]  
+	@rep_name = Hash[[[@sen_senator_title, @sen_senator_name], [@jun_senator_title, @jun_senator_name], [@rep_title, @rep_name]]]  
+	   if @rep_address[@recipient] != nil 
+	   @recipient_address = @rep_address[@recipient] 
+	   @recipient_lastname = @rep_lastname[@recipient] 
+	   @recipient_name = @rep_name[@recipient]	  
+	   end 
+ 
+   
     respond_to do |format|
-      format.html # show.html.erb
+      format.html #show.html.erb
       format.json { render json: @letter }
     end
   end
