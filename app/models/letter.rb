b31b8156eaf56c4159523c20f60eac7d22f9327a
Edward Castano <ed.castano@gmail.com>
2011-11-22 20:55:05 -0800

    Implemented Tableless and UI improvements

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,20 +1,5 @@
-class Letter 
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
+class Letter <Tableless 
+attr_accessor :name, :street_address, :city, :state, :zipcode
+validates_presence_of :name, :street_address, :city, :state, :zipcode, :presence => true
   
-  attr_accessor :name, :street_address, :city, :state, :zipcode
-  
-  validates_presence_of :name, :street_address, :city, :state, :zipcode
-  
-  def initialize(attributes = {})
-   attributes.each do |name, value|
-     send("#{name}=", value)
-	 end
-	end
-	
-	def persisted?
-	 false
-	end
-
 end
