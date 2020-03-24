21a34bf5c433372754fd9b041dfb4cf55e3bb002
3lviend <henryss_10@yahoo.com>
2020-03-24 16:58:09 +0700

    update routing and add assets precompile

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :shares
+
+  root "letters#new"
+  resources :sentences
+  resources :letters
+  get 'letter', to: "letters#show", :as => 'leatter'
+  get 'home', to: "letters#new"
+  get 'edit', to: "letters#edit"
 end
